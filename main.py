a1bbc27cfc9710583e89f01d60db4e49edae1af7
Yassir Karroum <ukarroum17@gmail.com>
2018-01-29 18:41:51 +0100

    Add ToDo section

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
-from ozzdesign import *
+from ozzdesign import OZZDesign
 
-exp = OZZDesign(25, filename="MC07_10000.txt")
+exp = OZZDesign(filename="MC07_10000.txt", nbExp=25)
 
-# exp.getMor(25)
+exp.readMor(filename="samples\\exp.txt")
+
+print(exp.meanMor())
+
+print(exp.varMor())
+# exp.getMor()
 #
-# exp.saveMor(filename="clu.txt")
-
-# exp.readMor(filename="samples\\exp.txt")
-#
-# print(exp.meanMor())
-
-
-
-#
-# exp.getOptDesign(3)
-#
-# exp.saveOpt(filename="samples\\1_act.txt")
-
-exp.readOpt(filename="samples\\exp.txt", ord=3)
-
-print(exp.meanOpt())
-
-#print(exp.meanMor())
+# exp.saveMor(filename="samples\\clusters.txt")
--- results.md
+++ results.md
@@ -14,4 +14,18 @@ mean(active_3) = 1.81657845957e-05
 
 Test avec toutes les variables
 
-mean(ref) = 
+mean(ref) =
+
+
+## ToDo
+
+* Ajouter guide pour utilisation
+* AJouter interface MatLab
+* Ajouter d'autres lois (cf NumPy) (surtout une log normal)
+* Choisir automatiquement nombre de clusters
+* AJouter moments d'ordre 3 4
+* Ajouter CDF et la PDF
+* Cf bootstraping
+* Regressiin linéaire inverse
+* 10 monte carlo (différents) et comparer clusters (25)
+* rapprocher le k-means des points initiaux
